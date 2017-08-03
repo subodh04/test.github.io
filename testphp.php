e1b4b1214d3b0c86c694a851d314b76cf10f6406
Sudhanshu Mishra <msudhanshu08@gmail.com>
2017-08-03 11:02:22 -0700

    test

---
+++ testphp.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+ Thank You <?php echo $_POST["firstname"]; ?><?php echo $_POST["lastname"]; ?>
+ <br>
+ We got your response and we are glad to know that you are intrested
+ you have selected your ROLE as : <?php echo $_POST["Role"]; ?><br>
+ Your Contact number is : <? echo $_POST["Contact"]; ?><br>
+ your email id is : <? echo $_POST["Email"]; ?><br>
+
+ If there is any error in above details please email on 
+ mishrasudhanshu398@gmail.com to correct it..
+ <br>
+ <br>
+
+ Thanking You!
+
+ </body>
+ </html> 
+
